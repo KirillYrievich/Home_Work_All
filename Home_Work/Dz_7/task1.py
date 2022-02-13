41d08a125730dfd47b7eb6a8e90b968adcd9e3a3
kirill <afanas.lg2@gmail.com>
2022-02-13 23:01:46 +0300

    Add files

---
+++ Home_Work/Dz_7/task1.py
@@ -0,0 +1,16 @@
+
+import os
+from pathlib import Path
+
+if not os.path.isdir('my_project'):
+    os.mkdir('my_project')
+
+os.chdir("my_project")
+new_dir = Path.cwd()
+
+def creating_folders(*names_folders):
+    for i in names_folders:
+        if not os.path.isdir(i):
+            os.mkdir(i)
+
+creating_folders('settings','mainapp','adminapp','authapp')
