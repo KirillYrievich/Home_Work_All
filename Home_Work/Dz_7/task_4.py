8ff3be02ebcd22c9d7ebe0238a3c91ed688c8f62
kirill <afanas.lg2@gmail.com>
2022-03-01 22:41:14 +0300

    Add files

--- Home_Work/Dz_7/task_4.py
+++ Home_Work/Dz_7/task_4.py
@@ -1,1 +1,31 @@
-print("hi")
+import os
+
+myDict = {}
+
+list_100 = []
+list_1000 = []
+list_10000 = []
+list_100000 = []
+
+path_dir = 'some_data'
+
+os.stat_result
+
+stat_info = [os.stat(name).st_size for name in os.scandir(path_dir)]
+
+for i in stat_info:
+    if i <= 100:
+        list_100.append(i)
+    if 100 < i < 1000:
+        list_1000.append(i)
+    elif 1000 < i < 10000:
+        list_10000.append(i)
+    else:
+        list_100000.append(i)
+
+myDict[100] = len(list_100)
+myDict[1000] = len(list_1000)
+myDict[10000] = len(list_10000)
+myDict[100000] = len(list_100000)
+
+print(myDict)
